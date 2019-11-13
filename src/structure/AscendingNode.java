a77aaa34a027279db6528b9d91d3820ec9b974c9
Eleonore <eleonore.burgaud@etu.univ-nantes.fr>
2019-11-13 11:20:21 +0100

    removed children related attributes

--- src/structure/AscendingNode.java
+++ src/structure/AscendingNode.java
@@ -7,17 +7,14 @@ import java.util.List;
 public class AscendingNode {
     private Integer value;
     private AscendingNode parent;
-    private Collection<AscendingNode> children;
 
     /**
      * Constructor
      * @param value
      * @param parent
-     * @param children
      */
-    public AscendingNode(Integer value, AscendingNode parent, List<AscendingNode> children){
+    public AscendingNode(Integer value, AscendingNode parent){
         this.value = value;
-        this.children = children;
         this.parent = parent;
     }
 
@@ -45,12 +42,6 @@ public class AscendingNode {
         return this.parent;
     }
 
-    protected Collection<AscendingNode> getChildren(){
-        if (this.children.isEmpty()){
-            throw new NullPointerException("This unionFind.node has no children.");
-        }
-        return this.children;
-    }
 
     protected AscendingNode getRoot(){
         if(!this.hasParent()){
@@ -82,13 +73,6 @@ public class AscendingNode {
         this.parent = parent;
     }
 
-    /**
-     * set a new set of children
-     * @param children
-     */
-    protected void setChildren(List<AscendingNode> children){
-        this.children = children;
-    }
 
     /**
      * VERIFICATIONS
@@ -96,13 +80,6 @@ public class AscendingNode {
 
     protected boolean isRoot(){
         return !hasParent();
-    }
-
-    protected boolean isLeaf(){
-        return !hasChild();
-    }
-    private boolean hasChild(){
-        return !this.children.isEmpty();
     }
 
      private boolean hasParent(){
