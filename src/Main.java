4f71f5e9f4cccf74b7b418267622e31bfcd212a2
Eleonore <eleonore.burgaud@etu.univ-nantes.fr>
2019-11-21 19:52:03 +0100

    Modifications for 2D array

--- src/Main.java
+++ src/Main.java
@@ -8,37 +8,47 @@ public class Main {
     }
 
     private static void testingUnionFind() {
-        AscendingNode root1 = new AscendingNode(1);
+        AscendingNode root1 = new AscendingNode(1,0,0);
+        AscendingNode child1 = new AscendingNode(2,0,1, root1);
+        AscendingNode child2 = new AscendingNode(3,0,2,root1);
+        AscendingNode child3 = new AscendingNode(4,1,0, child1);
+        AscendingNode child4 = new AscendingNode(5,1,1, child3);
 
-        AscendingNode child1 = new AscendingNode(2, root1);
-        AscendingNode child2 = new AscendingNode(3, root1);
-        AscendingNode child3 = new AscendingNode(4, child1);
-        AscendingNode child4 = new AscendingNode(5, child3);
+        AscendingNode root2 = new AscendingNode(6,1,2);
+        AscendingNode child5 = new AscendingNode(7,2,0, root2);
+        AscendingNode child6 = new AscendingNode(8,2,1, root2);
+        AscendingNode child7 = new AscendingNode(9,2,2, child6);
 
-        AscendingNode root2 = new AscendingNode(6);
-        AscendingNode child5 = new AscendingNode(7, root2);
+        AscendingNode[][] a = new AscendingNode[3][3];
+        a[0][0] = root1.getParent();
+        a[0][1] = child1.getParent();
+        a[0][2] = child2.getParent();
+        a[1][0] = child3.getParent();
+        a[1][1] = child4.getParent();
+        a[1][2] = root2.getParent();
+        a[2][0] = child5.getParent();
+        a[2][1] = child6.getParent();
+        a[2][2] = child7.getParent();
 
-        AscendingNode[] a = new AscendingNode[7];
 
         UnionFind unionFind = new UnionFind(a);
 
-        a[0] = root1.getParent();
-        a[1] = child1.getParent();
-        a[2] = child2.getParent();
-        a[3] = child3.getParent();
-        a[4] = child4.getParent();
-        a[5] = root2.getParent();
-        a[6] = child5.getParent();
 
         System.out.println("n : parent");
         for(int i = 0; i< a.length; i++){
-            if(a[i] != null) System.out.println(i+1 +" : " + a[i].getValue());
+            for(int j = 0; j< a.length; j++){
+            if(a[i] != null) System.out.println("(" +i +", "+j + ") : " + a[i][j].getValue());
+            }
         }
         System.out.println("********");
 
-        unionFind.union(a[3],a[6]);
-        for(int i = 0; i< a.length; i++){
-            if(a[i] != null) System.out.println(i+1 +" : " + a[i].getValue());
-        }
+        unionFind.union(a[1][1],a[2][2]);
+
+            for(int i = 0; i< a.length; i++) {
+                for (int j = 0; j < a.length; j++) {
+                    if (a[i] != null) System.out.println("(" +i +", "+j + ") : " + a[i][j].getValue());
+                }
+            }
+
     }
 }
--- src/structure/AscendingNode.java
+++ src/structure/AscendingNode.java
@@ -2,7 +2,10 @@ package structure;
 
 
 public class AscendingNode {
+
     private int value;
+    private int x;
+    private int y;
 
     private AscendingNode parent;
 
@@ -10,10 +13,14 @@ public class AscendingNode {
      * Constructor
      *
      * @param value
+     * @param x
+     * @param y
      * @param parent
      */
-    public AscendingNode(Integer value, AscendingNode parent) {
+    public AscendingNode(int value,int x, int y, AscendingNode parent) {
         this.value = value;
+        this.x = x;
+        this.y = y;
         this.parent = parent;
     }
 
@@ -22,8 +29,10 @@ public class AscendingNode {
      *
      * @param value
      */
-    public AscendingNode(Integer value) {
+    public AscendingNode(int value,int x,int y) {
         this.value = value;
+        this.x = x;
+        this.y = y;
         setParent(this);
     }
 
@@ -39,6 +48,23 @@ public class AscendingNode {
      */
     public int getValue() {
         return this.value;
+    }
+
+    /**
+     * get value of current node
+     *
+     * @return value
+     */
+    public int getX() {
+        return this.x;
+    }
+    /**
+     * get value of current node
+     *
+     * @return value
+     */
+    public int getY() {
+        return this.y;
     }
 
     /**
@@ -67,15 +93,6 @@ public class AscendingNode {
     /**
      * SETTERS
      */
-
-    /**
-     * set a new value
-     *
-     * @param value
-     */
-    protected void setValue(int value) {
-        this.value = value;
-    }
 
     /**
      * set a new parent
